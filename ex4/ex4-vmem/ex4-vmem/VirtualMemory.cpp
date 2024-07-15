468c907e2b68eebe76ceb868106a09d3cf7fa75b
Nimrod Mallis <nimrodmallis@gmail.com>
2024-07-15 10:26:27 +0300

    Created project and moved files

---
+++ ex4/ex4-vmem/ex4-vmem/VirtualMemory.cpp
@@ -0,0 +1,12 @@
+﻿// ex4-vmem.cpp : Defines the entry point for the application.
+//
+
+#include "ex4-vmem.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
